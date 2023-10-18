5812f298d7742d04256243d6f2b6aa968272f9e6
Alberto Martín Sánchez <vhiek.ams@gmail.com>
2023-10-18 23:58:06 +0200

    Añadir update a la api

--- src/main/java/com/alberto/demo/student/StudentController.java
+++ src/main/java/com/alberto/demo/student/StudentController.java
@@ -3,6 +3,10 @@ package com.alberto.demo.student;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping(path = "api/v1/student")
@@ -27,5 +31,13 @@ public class StudentController {
     @DeleteMapping(path = "{studentId}")
     public void deleteStudent(@PathVariable("studentId") Long studentId) {
         studentService.deleteStudent(studentId);
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent (
+        @PathVariable("studentId") Long studentId,
+        @RequestParam(required = false) String name,
+        @RequestParam(required = false) String email) {
+        studentService.updateStudent(studentId, name, email);
     }
 }
--- src/main/java/com/alberto/demo/student/StudentService.java
+++ src/main/java/com/alberto/demo/student/StudentService.java
@@ -1,10 +1,13 @@
 package com.alberto.demo.student;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class StudentService {
@@ -35,5 +38,22 @@ public class StudentService {
         }
         studentRepository.deleteById(studentId);
     }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId).orElseThrow(() -> new IllegalStateException("El estudiante con id " + studentId + " no existe"));
+        if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
+            student.setName(name);
+        }
+
+        if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if (studentOptional.isPresent()) {
+                throw new IllegalStateException("Este email ya está en uso");
+        }
+        student.setEmail(email);
+
+    }
+}
     
 }
